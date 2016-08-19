c0a788eab809bec4362f0294116fb4602c881dce
petcareclinic <arunkumarv.ras@gmail.com>
2016-08-19 23:19:08 +0530

    Add files via upload

---
+++ HCare/src/java/com/thamiyan/constants/Constants.java
@@ -0,0 +1,18 @@
+package com.thamiyan.constants;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Madhankumar
+ */
+public class Constants {
+    
+    public static final String COMPANY_NAME="Thamiyan";
+    
+    
+}
